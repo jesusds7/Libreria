dde44f4052854710d9b767edc25aaa2b20d21158
david <david@DavidGuio>
2015-05-31 23:13:40 -0500

    cambios

--- Libreria/src/persistencia/LeerXmlLibros.java
+++ Libreria/src/persistencia/LeerXmlLibros.java
@@ -19,31 +19,14 @@ public class LeerXmlLibros {
 		    DocumentBuilderFactory fábricaCreadorDocumento = DocumentBuilderFactory.newInstance();
 		    DocumentBuilder creadorDocumento = fábricaCreadorDocumento.newDocumentBuilder();
 		    Document documento = creadorDocumento.parse("src/data/libros.xml");
-		    //Obtener el elemento raíz del documento
+		   
 		    Element raiz = documento.getDocumentElement();
 
-		    //Obtener la lista de nodos que tienen etiqueta "EMPLEADO"
-		    NodeList listaEmpleados = raiz.getElementsByTagName("Libro");
-		    libros = listaEmpleados.getLength();
+		   
+		    NodeList listaLibros = raiz.getElementsByTagName("Libro");
+		    libros = listaLibros.getLength();
 		  
-		    //Recorrer la lista de empleados
-//		    for(int i=0; i<listaEmpleados.getLength(); i++) {   
-//		        //Obtener de la lista un empleado tras otro
-//		        Node empleado = listaEmpleados.item(i);
-//		    
-//		        
-//		        System.out.println("Empleado "+i);
-//		        System.out.println("==========");    
-//
-//		        //Obtener la lista de los datos que contiene ese empleado
-//		        NodeList datosEmpleado = empleado.getChildNodes();
-//		        //Recorrer la lista de los datos que contiene el empleado
-//		        for(int j=0; j<datosEmpleado.getLength(); j++) {
-//		           this.libros=j;
-//		                 
-//		        }
-//		       
-//		    }
+
 		 
 		} catch (SAXException ex) {
 		    System.out.println("ERROR: El formato XML del fichero no es correcto\n"+ex.getMessage());
@@ -56,7 +39,7 @@ public class LeerXmlLibros {
 		    ex.printStackTrace();
 		}
 		  return libros;
-		// return libros;
+		
 	}
 
 }
